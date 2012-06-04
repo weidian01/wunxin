72077d2557c334eef521041147bce8b48bdb03a4
weidian01@gmail.com <weidian01@gmail.com@256c6253-8d44-2265-c686-efb5c910714f>
2012-06-04 06:09:12 +0000

    123

--- trunk/system/core/Model.php
+++ trunk/system/core/Model.php
@@ -1,15 +1,15 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 /**
  * CodeIgniter
  *
  * An open source application development framework for PHP 5.1.6 or newer
  *
- * @package		CodeIgniter
- * @author		ExpressionEngine Dev Team
- * @copyright	Copyright (c) 2008 - 2011, EllisLab, Inc.
- * @license		http://codeigniter.com/user_guide/license.html
- * @link		http://codeigniter.com
- * @since		Version 1.0
+ * @package        CodeIgniter
+ * @author        ExpressionEngine Dev Team
+ * @copyright    Copyright (c) 2008 - 2011, EllisLab, Inc.
+ * @license        http://codeigniter.com/user_guide/license.html
+ * @link        http://codeigniter.com
+ * @since        Version 1.0
  * @filesource
  */
 
@@ -18,50 +18,69 @@
 /**
  * CodeIgniter Model Class
  *
- * @package		CodeIgniter
- * @subpackage	Libraries
- * @category	Libraries
- * @author		ExpressionEngine Dev Team
- * @link		http://codeigniter.com/user_guide/libraries/config.html
+ * @package        CodeIgniter
+ * @subpackage    Libraries
+ * @category    Libraries
+ * @author        ExpressionEngine Dev Team
+ * @link        http://codeigniter.com/user_guide/libraries/config.html
  */
-class CI_Model {
+class CI_Model
+{
 
-	/**
-	 * Constructor
-	 *
-	 * @access public
-	 */
-	function __construct()
-	{
+    /**
+     * Constructor
+     *
+     * @access public
+     */
+    function __construct()
+    {
         $this->load->database();
 
-		log_message('debug', "Model Class Initialized");
-	}
+        log_message('debug', "Model Class Initialized");
+    }
 
-	/**
-	 * __get
-	 *
-	 * Allows models to access CI's loaded classes using the same
-	 * syntax as controllers.
-	 *
-	 * @param	string
-	 * @access private
-	 */
-	function __get($key)
-	{
-		$CI =& get_instance();
-		return $CI->$key;
-	}
+    /**
+     * __get
+     *
+     * Allows models to access CI's loaded classes using the same
+     * syntax as controllers.
+     *
+     * @param    string
+     * @access private
+     */
+    function __get($key)
+    {
+        $CI =& get_instance();
+        return $CI->$key;
+    }
+
+
+//    function __destruct()
+//    {
+//        //*
+//        if (ENVIRONMENT === 'development' && get_called_class() !== 'CI_Model') {
+//            echo '<pre>';
+//            echo 'queries : ', $this->db->query_count;
+//            echo '<br>';
+//            print_r($this->db->queries);
+//            echo '</pre>';
+//        }
+//        //*/
+//    }
+
 
     function __destruct()
     {
         //*
-        echo '<pre>';
-        foreach ($this->db->queries as $k=>$v) {
-            echo '<b style="color:red;font-size:20px;">SQL:</b>'.str_replace("\n", '', $v).' ------ <b style="color:red;font-size:20px;">TIME:</b>'.$this->db->query_times[$k].'<br>';
+        if (ENVIRONMENT === 'development' && get_called_class() !== 'CI_Model') {
+            echo '<pre>';
+            foreach ($this->db->queries as $k => $v) {
+                echo '<b style="color:red;font-size:20px;">SQL:</b>' . str_replace("\n", '', $v) . ' ------ <b style="color:red;font-size:20px;">TIME:</b>' . $this->db->query_times[$k];
+            }
         }
         //*/
     }
+
 }
 // END Model Class
 
